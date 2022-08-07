e38768072c637a073d095e368d7e2a596e5c22c0
Doug Flynn <DouglassHooker@gmail.com>
2022-08-07 18:37:36 -0400

    Update schema with requried fields

--- schemas/album.js
+++ schemas/album.js
@@ -1,3 +1,5 @@
+export const createPlayId = (_) => crypto.randomUUID();
+
 export default {
   name: "album",
   type: "document",
@@ -7,25 +9,36 @@ export default {
       name: "albumArt",
       type: "image",
       title: "Album Art",
-    },
-    {
-      name: "playId",
-      type: "slug",
-      title: "Play ID",
-      options: {
-        source: "title",
-        slugify: (_) => crypto.randomUUID(),
-      },
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "artist",
       type: "string",
       title: "Artist Name",
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "title",
       type: "string",
       title: "Album Title",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "isSingle",
+      type: "boolean",
+      title: "Is Single",
+      initialValue: false,
+    },
+    {
+      name: "playId",
+      type: "slug",
+      title: "Play ID",
+      description: "Unique identifier for this album, DO NOT CHANGE :)",
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: "title",
+        slugify: createPlayId,
+      },
     },
   ],
 };
